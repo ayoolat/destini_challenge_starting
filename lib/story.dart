c0914bf1abc619e5d6b322df27be1cc655bf6761
ayoolat <ayoola_toluwanimi@yahoo.com>
2022-10-31 18:48:38 +0100

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+
+  Story({required storyTitle, required choice1, required choice2}) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
